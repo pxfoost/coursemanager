d304b36633586826a3b47ddb42bfa4eae3845ef5
hasee <1969313704@qq.com>
2019-01-11 15:59:34 +0800

    third commit

--- app/src/main/java/com/kth/groups/coursemanager/CourseModifyActivity.java
+++ app/src/main/java/com/kth/groups/coursemanager/CourseModifyActivity.java
@@ -2,15 +2,12 @@ package com.kth.groups.coursemanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
-public class LoginActivity extends AppCompatActivity {
+public class CourseModifyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        Log.d("this","loginactivity");
+        setContentView(R.layout.activity_course_modify);
     }
 }
