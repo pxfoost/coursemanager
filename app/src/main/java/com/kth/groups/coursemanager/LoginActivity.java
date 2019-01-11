96b7411573ce55b3a08a617ac851b1c2b4085265
hasee <1969313704@qq.com>
2019-01-11 09:24:44 +0800

    add log in LoginActivity

--- app/src/main/java/com/kth/groups/coursemanager/LoginActivity.java
+++ app/src/main/java/com/kth/groups/coursemanager/LoginActivity.java
@@ -2,6 +2,7 @@ package com.kth.groups.coursemanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_layout);
+
+        Log.d("this","loginactivity");
     }
 }
